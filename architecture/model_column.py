a272ac8a2547a9bf0cecc36c5ef0cddaf7b09015
Chris Konishi <christian.konishi@gmail.com>
2021-06-12 10:33:14 -0300

    change skip channel for 3rd try

--- architecture/model_column.py
+++ architecture/model_column.py
@@ -14,11 +14,11 @@ class Column_U(nn.Module):
             , Conv2d(2*first_out_chn, first_out_chn, kernel_size, same_padding=True, bn=bn)
             , Conv2d(first_out_chn, int(first_out_chn/2), kernel_size, same_padding=True, bn=bn)
         )
-        self.up_1 = Upsample(int(first_out_chn/2), int(first_out_chn/4), int(first_out_chn/4), int(first_out_chn/4), kernel_size=kernel_size, bn=bn)
+        self.up_1 = Upsample(int(first_out_chn/2), int(first_out_chn/2), int(first_out_chn/2), int(first_out_chn/4), kernel_size=kernel_size, bn=bn)
         self.up_2 = Upsample(int(first_out_chn/4), int(first_out_chn/4), int(first_out_chn/4), int(first_out_chn/4), kernel_size=kernel_size, bn=bn)
 
         self.skip_1 = Conv2d(first_out_chn, int(first_out_chn/4), 1, same_padding=True, bn=bn)
-        self.skip_2 = Conv2d(2*first_out_chn, int(first_out_chn/4), 1, same_padding=True, bn=bn)
+        self.skip_2 = Conv2d(2*first_out_chn, int(first_out_chn/2), 1, same_padding=True, bn=bn)
 
     def forward(self, x):
         hold_1 = self.entry(x)
